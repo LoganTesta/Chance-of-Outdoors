07d7cf7c5449c2c61198839f5f98cd66673956d5
logan <logan.testa@outlook.com>
2023-10-09 22:07:58 -0700

    Adjust title section styling

--- wordpress/wp-content/themes/twentytwentyone-child/functions.php
+++ wordpress/wp-content/themes/twentytwentyone-child/functions.php
@@ -9,8 +9,8 @@ add_action( 'after_setup_theme', 'outdoor_supply_store_setup' );
 
 
 add_action( 'wp_enqueue_scripts', function() {
-    wp_enqueue_style( 'child-style', get_stylesheet_uri() . "?mod=10022023", array( 'twenty-twenty-one-style' ) );
-    wp_enqueue_style( 'print-styles', "" . get_stylesheet_directory_uri() . '/assets/css/print-styles.css?mod=10022023', array(), '', 'print' );
+    wp_enqueue_style( 'child-style', get_stylesheet_uri() . "?mod=10092023", array( 'twenty-twenty-one-style' ) );
+    wp_enqueue_style( 'print-styles', "" . get_stylesheet_directory_uri() . '/assets/css/print-styles.css?mod=10092023', array(), '', 'print' );
     
     //Add Slick.js
     wp_enqueue_style( 'slick-css', "" . get_stylesheet_directory_uri() . '/assets/css/slick.css', '1.8.1', true );   
