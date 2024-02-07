979ec0c531aaaef873e682566dec91046e91bad2
logan <logan.testa@outlook.com>
2024-02-06 22:01:11 -0800

    Set footer nav and social menus in columns

--- wordpress/wp-content/themes/twentytwentyone-child/footer.php
+++ wordpress/wp-content/themes/twentytwentyone-child/footer.php
@@ -19,46 +19,48 @@
 	<?php get_template_part( 'template-parts/footer/footer-widgets' ); ?>
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
+                <div class="footer-content-section">
+                        <?php if ( has_nav_menu( 'footer-nav-links' ) ) : ?>
+                                <nav aria-label="<?php esc_attr_e( 'Footer Nav Links', 'twentytwentyone' ); ?>" class="footer-navigation footer-section">
+                                        <ul class="footer-navigation-inner-wrapper">
+                                                <?php
+                                                wp_nav_menu(
+                                                        array(
+                                                                'theme_location' => 'footer-nav-links',
+                                                                'items_wrap'     => '%3$s',
+                                                                'container'      => false,
+                                                                'depth'          => 1,
+                                                                'link_before'    => '<span>',
+                                                                'link_after'     => '</span>',
+                                                                'fallback_cb'    => false,
+                                                        )
+                                                );
+                                                ?>
+                                        </ul><!-- .footer-navigation-wrapper -->
+                                </nav><!-- .footer-navigation -->
+                        <?php endif; ?>
 
-                <?php if ( has_nav_menu( 'footer-nav-links' ) ) : ?>
-			<nav aria-label="<?php esc_attr_e( 'Footer Nav Links', 'twentytwentyone' ); ?>" class="footer-navigation">
-				<ul class="footer-navigation-wrapper">
-					<?php
-					wp_nav_menu(
-						array(
-							'theme_location' => 'footer-nav-links',
-							'items_wrap'     => '%3$s',
-							'container'      => false,
-							'depth'          => 1,
-							'link_before'    => '<span>',
-							'link_after'     => '</span>',
-							'fallback_cb'    => false,
-						)
-					);
-					?>
-				</ul><!-- .footer-navigation-wrapper -->
-			</nav><!-- .footer-navigation -->
-		<?php endif; ?>
-                        
-		<?php if ( has_nav_menu( 'footer' ) ) : ?>
-			<nav aria-label="<?php esc_attr_e( 'Secondary menu', 'twentytwentyone' ); ?>" class="footer-navigation">
-				<ul class="footer-navigation-wrapper">
-					<?php
-					wp_nav_menu(
-						array(
-							'theme_location' => 'footer',
-							'items_wrap'     => '%3$s',
-							'container'      => false,
-							'depth'          => 1,
-							'link_before'    => '<span>',
-							'link_after'     => '</span>',
-							'fallback_cb'    => false,
-						)
-					);
-					?>
-				</ul><!-- .footer-navigation-wrapper -->
-			</nav><!-- .footer-navigation -->
-		<?php endif; ?>
+                        <?php if ( has_nav_menu( 'footer' ) ) : ?>
+                                <nav aria-label="<?php esc_attr_e( 'Secondary menu', 'twentytwentyone' ); ?>" class="footer-navigation footer-section">
+                                        <ul class="footer-navigation-wrapper">
+                                                <?php
+                                                wp_nav_menu(
+                                                        array(
+                                                                'theme_location' => 'footer',
+                                                                'items_wrap'     => '%3$s',
+                                                                'container'      => false,
+                                                                'depth'          => 1,
+                                                                'link_before'    => '<span>',
+                                                                'link_after'     => '</span>',
+                                                                'fallback_cb'    => false,
+                                                        )
+                                                );
+                                                ?>
+                                        </ul><!-- .footer-navigation-wrapper -->
+                                </nav><!-- .footer-navigation -->
+                        <?php endif; ?>
+                       <div class="clear-both"></div>
+                </div>
 		<div class="site-info">
 			<div class="site-name">
 				<?php if ( has_custom_logo() ) : ?>
