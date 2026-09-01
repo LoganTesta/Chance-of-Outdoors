0f7ad13b0c49c25858f2675bbb7aa94e7f064e2a
logan <logan.testa@outlook.com>
2026-08-31 22:37:16 -0700

    Add back to top button in footer

--- wordpress/wp-content/themes/twentytwentyone-child/functions.php
+++ wordpress/wp-content/themes/twentytwentyone-child/functions.php
@@ -15,8 +15,8 @@ add_action( 'init', 'register_new_menus' );
 
 
 add_action( 'wp_enqueue_scripts', function() {
-    wp_enqueue_style( 'child-style', get_stylesheet_uri() . "?mod=12122025", array( 'twenty-twenty-one-style' ) );
-    wp_enqueue_style( 'print-styles', "" . get_stylesheet_directory_uri() . '/assets/css/print-styles.css?mod=12122025', array(), '', 'print' );
+    wp_enqueue_style( 'child-style', get_stylesheet_uri() . "?mod=08312026", array( 'twenty-twenty-one-style' ) );
+    wp_enqueue_style( 'print-styles', "" . get_stylesheet_directory_uri() . '/assets/css/print-styles.css?mod=08312026', array(), '', 'print' );
     
     wp_register_script( 'javascript-functions', get_stylesheet_directory_uri() . '/assets/javascript/javascript-functions.js' );
     wp_enqueue_script( 'javascript-functions', get_stylesheet_directory_uri() . '/assets/javascript/javascript-functions.js' ); 
